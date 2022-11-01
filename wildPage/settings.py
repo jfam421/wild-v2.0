b1608ab576cd95a0d2ef1505a4c0b83729c54420
jfam421 <30318199+jfam421@users.noreply.github.com>
2022-11-01 21:40:41 +0200

    Changes

--- wildPage/settings.py
+++ wildPage/settings.py
@@ -95,7 +95,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'wildPage.urls'
 CSRF_TRUSTED_ORIGINS = ['https://wild-b.herokuapp.com']
-CORS_ALLOWED_ORIGINS = ['https://wild-b.herokuapp.com', "http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = ['https://wild-b.herokuapp.com', "http://127.0.0.1:3000"]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
